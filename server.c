f8b20b67b902ecb0c8e1c194f029ac6c0d21d8dd
Abdeljalil <abdejalildanane@gmail.com>
2026-09-22 13:53:50 +0000

    Add snsprintf

--- server.c
+++ server.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <sys/socket.h>
-
-#include <unistd.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 int main()
 {
@@ -12,7 +13,7 @@ int main()
 
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(8080);
+    server_address.sin_port = htons(8083);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -26,8 +27,59 @@ int main()
         return 1;
     }
 
-    printf("Succss %d", server_fd);
+    if (listen(server_fd, 10) == -1)
+    {
+
+        perror("lisen failed");
+        close(server_fd);
+        return 1;
+    }
+
+    int client_fd;
+    client_fd = accept(server_fd, NULL, NULL);
+
+    if (client_fd == -1)
+    {
+        perror("lisen failed");
+        close(server_fd);
+        return 1;
+    }
+
+    char buffer[4096];
+
+    int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+    if (bytes_received == -1)
+    {
+        perror("recv failed");
+        close(server_fd);
+        return 1;
+    }
+
+    buffer[bytes_received] = '\0';
+
+    char response[1024] = "HTTP/1.1 200 OK\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: %d\r\n"
+                          "Connection: close\r\n"
+                          "\r\n"
+                          "%s";
+
+    char *body =
+        "<h1>Welcome</h1>"
+        "<h2 style=\"color:red\">This is subtitle</h2>";
+    size_t body_length = strlen(body);
+
+    snprintf(response, sizeof(response), response, body_length, body);
+
+    send(client_fd, response, strlen(response), 0);
+
+    printf("Browser connected! Client FD: %d\n", client_fd);
+
+    printf("Server run on http://localhost:8083 \n");
+
     close(server_fd);
+    close(client_fd);
 
     return 0;
 }
